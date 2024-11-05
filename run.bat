95761e468d21fb5b0b2a159a873d97fefac07bf7
klimdanick <klimdanick@gmail.com>
2024-11-06 00:57:25 +0100

    fix

--- run.bat
+++ run.bat
@@ -3,6 +3,15 @@ setlocal
 
 cd /d %~dp0
 
+git pull
+
+:: Install npm dependencies
+call npm install
+if errorlevel 1 (
+    echo npm install failed.
+    exit /b %errorlevel%
+)
+
 :: Start the application
 npm start
 if errorlevel 1 (
